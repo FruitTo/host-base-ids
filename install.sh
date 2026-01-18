2a3811fd01dd2099c2177aa6de8b2d56747c3336
FRUITTO <krissada.22447@gmail.com>
2026-01-18 18:13:58 +0700

    docker install .sh

--- install.sh
+++ install.sh
@@ -4,14 +4,16 @@ echo "[*] install dependencies"
 sudo apt update
 sudo apt install -y build-essential cmake git libpcap-dev libssl-dev libtins-dev libpqxx-dev libcurl4-openssl-dev at
 
-# json library
-git clone https://github.com/nlohmann/json.git /tmp/json
-cd /tmp/json
-mkdir build
-cd build
-cmake ..
-sudo make install
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-rm -rf /tmp/json
+# # json library
+# git clone https://github.com/nlohmann/json.git /tmp/json
+# cd /tmp/json
+# mkdir build
+# cd build
+# cmake ..
+# sudo make install
+
+# rm -rf /tmp/json
 
 echo "[*] install done."
